08e38773ec06d9d21c560814e47cf20d69ba3762
wamakimaN <wamamoses@gmail.com>
2020-02-16 20:35:30 +0300

    add pfofile model

--- projects/models.py
+++ projects/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Profile()
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete = models.CASCADE)
+  bio = models.TextField(max_length = 30, blank = True)
+  profile_pic = models.ImageField(default= 'default.jpg', upload_to='profile_pics')
+  
+
+
