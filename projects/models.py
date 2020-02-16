21c2a5b625129cb2171e4599f3843a179230dc00
wamakimaN <wamamoses@gmail.com>
2020-02-16 20:36:15 +0300

    add post model

--- projects/models.py
+++ projects/models.py
@@ -6,6 +6,10 @@ class Profile(models.Model):
   user = models.OneToOneField(User, on_delete = models.CASCADE)
   bio = models.TextField(max_length = 30, blank = True)
   profile_pic = models.ImageField(default= 'default.jpg', upload_to='profile_pics')
-  
 
-
+class Post(models.Model):
+  profile = models.ForeignKey(Profile, null = True, blank = True)
+  name = models.CharField(max_length = 60)
+  image = models.ImageField(upload_to='post_pics')
+  site = models.URLField(null = True)
+  description = TextField(max_length = 200)
