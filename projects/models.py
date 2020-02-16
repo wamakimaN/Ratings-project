5fbf0858684e191df01cc9a958b9c1efee1548de
wamakimaN <wamamoses@gmail.com>
2020-02-16 20:36:42 +0300

    add rating model

--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Profile(models.Model):
@@ -11,5 +12,10 @@ class Post(models.Model):
   profile = models.ForeignKey(Profile, null = True, blank = True)
   name = models.CharField(max_length = 60)
   image = models.ImageField(upload_to='post_pics')
-  site = models.URLField(null = True)
+  site = models.URLField(max_length = 60)
   description = TextField(max_length = 200)
+
+class Rating(models.Model):
+  post = ForeignKey(Post, on_delete = models.CASCADE)
+  user = models.ForeignKey(User, on_delete = models.CASCADE)
+  stars = models.IntegerField(validators = [MinValueValidator(1),MaxValueValidator(5)])
