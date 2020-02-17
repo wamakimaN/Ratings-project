8992a27a28ccba4dabc936cd923c1be18cbcbdf7
wamakimaN <wamamoses@gmail.com>
2020-02-17 10:45:38 +0300

    add post functiion for our api

--- projects/views.py
+++ projects/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import  Rating
 from .serializer import RatingSerializer
+from rest_framework import status
 
 
 # Create your views here.
@@ -11,3 +12,10 @@ class RatingList(APIView):
     ratings = Rating.objects.all()
     serializers = RatingSerializer(ratings, many=True)
     return Response(serializers.data)
+
+  def post(self, request, format = None):
+    serializers = RatingSerializer(data=request.data)
+    if serializers.is_valid():
+      serializers.save()
+      return Response(serializers.data, status=status.HTTP_201_CREATED)
+    return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
