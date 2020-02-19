2720bf9f6079452cdf0cd81693af929458134d60
wamakimaN <wamamoses@gmail.com>
2020-02-19 10:28:27 +0300

    create base html file

--- projects/urls.py
+++ projects/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
   url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
   url(r'^logout/$', auth_views.logout, {"next_page": '/'}, name='logout'),
   url(r'^signup/$',views.registration, name='signup'),
-  # url(r'^rate/$', views.PostCreate.as_view(), name='rate'),
+  url(r'^rate/$', views.PostCreate.as_view(), name='rate'),
   url(r'^post/$', views.PostCreate.as_view(), name='post_new'),
   url(r'^$', views.PostView.as_view(), name='home'),
   url(r'^details/(?P<id>[\d]+)/', views.PostDetailView.as_view(), name='details'),
